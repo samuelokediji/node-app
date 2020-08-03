6f0a3d33dd5e3b693a31e5d4db761f18d8372308
samuelokediji <samuelolaideokediji@gmail.com>
2020-08-03 03:12:12 +0100

    vue app

---
+++ src/components/Users.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="app">
+    <Users />
+  </div>
+</template>
+<script>
+import Users from './components/Users.vue'
+export default {
+  name: 'app',
+  components: {
+    Users
+  }
+}
+
+ import axios from 'axios';
+  export default {
+    name: 'Users'
+  }
+
+  [
+{
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  },
+  {
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "email": "Shanna@melissa.tv",
+    "address": {
+      "street": "Victor Plains",
+      "suite": "Suite 879",
+      "city": "Wisokyburgh",
+      "zipcode": "90566-7771",
+      "geo": {
+        "lat": "-43.9509",
+        "lng": "-34.4618"
+      }
+    },
+    "phone": "010-692-6593 x09125",
+    "website": "anastasia.net",
+    "company": {
+      "name": "Deckow-Crist",
+      "catchPhrase": "Proactive didactic contingency",
+      "bs": "synergize scalable supply-chains"
+    }
+  },
+  
+  import axios from 'axios';
+  export default {
+    name: 'Users',
+    data() {
+      return {
+        users: null,
+      };
+    },
+    created: function() {
+      axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then(res => {
+          this.users = res.data;
+        })
+    }
+  }
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
